c04d7db464f1526503a73d2a157545aa4767f4cc
Karan162040 <kumar.karan.162040@gmail.com>
2024-04-21 13:30:45 +0530

    Displaying cart

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import "./App.css";
+import React from "react";
 import Header from "./components/Layout/Header";
 import Colors from "./components/Colors/Colors";
+import Cart from "./components/Cart/Cart";
+import { useState } from "react";
 
 function App() {
+  const [modalShow, setModalShow] = React.useState(true);
+  const cartShow = () => {
+    setModalShow(true);
+  };
+  const cartHide = () => {
+    setModalShow(false);
+  };
   return (
     <div>
-      <Header />
+      <Header OnShow={cartShow} />
       <Colors />
+      {modalShow && <Cart OnHide={cartHide} show={modalShow} />}
     </div>
   );
 }
-
 export default App;
--- src/components/UI/ModalUI.js
+++ src/components/UI/ModalUI.js
@@ -1,1 +1,16 @@
-const ModalUI = (props) => [];
+import { Offcanvas } from "react-bootstrap";
+
+function ModalUI(props) {
+  return (
+    <div>
+      <Offcanvas show={props.show} onHide={props.OnHide} placement="end">
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>{props.children}</Offcanvas.Body>
+      </Offcanvas>
+    </div>
+  );
+}
+
+export default ModalUI;
